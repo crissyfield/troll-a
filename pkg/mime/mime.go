7a48ba8c4cff0b416ada41634a132eb516e0e94f
Thomas Jansen <thomas@crissyfield.de>
2023-12-18 13:23:07 +0100

    Make mime type detection more lenient

--- pkg/mime/mime.go
+++ pkg/mime/mime.go
@@ -1,12 +1,17 @@
 package mime
 
 import (
+	"strings"
+
 	"github.com/gabriel-vasile/mimetype"
 )
 
 const (
-	// textPlainMimeType specifies the mime type of "text/plain".
-	textPlainMimeType = "text/plain"
+	// textMimeType specifies the mime type family of "text".
+	textMimeType = "text"
+
+	// textPlainMimeSubtype specifies the mime subtype of "text/plain".
+	textPlainMimeSubtype = "text/plain"
 )
 
 var (
@@ -30,9 +35,22 @@ func IsText(mime string) bool {
 
 // isTextNoCache returns true if the given mime is inherited from "text/plain". Any cache is ignored.
 func isTextNoCache(mime string) bool {
+	// Remove additional information
+	parts := strings.SplitN(mime, ";", 2)
+	if len(parts) < 1 {
+		return false
+	}
+
+	mime = strings.ToLower(strings.TrimSpace(parts[0]))
+
+	// Early exit if already text mime type
+	if strings.HasPrefix(mime, textMimeType+"/") {
+		return true
+	}
+
 	// Traverse mime tree
 	for mt := mimetype.Lookup(mime); mt != nil; mt = mt.Parent() {
-		if mt.Is(textPlainMimeType) {
+		if mt.Is(textPlainMimeSubtype) {
 			return true
 		}
 	}
